bee4b46409a12769790f9824fa49fc150b9757fe
Pallavikothawade <Pallavijkothawade@gmail.com>
2023-01-01 22:34:19 +0530

    Displaying Details Of The Person UC1_DisplayThePerson

---
+++ ProgramAddressBook/AddressBook.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramAddressBook
+{
+    internal class AddressBook
+    {
+        List<Contacts> add = new List<Contacts>();
+        public void Addbook()
+        {
+            
+            Contacts contacts1 = new Contacts();
+            Console.WriteLine("Enter The FirstName");
+            contacts1.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter The LastName");
+            contacts1.LastName = Console.ReadLine();
+            Console.WriteLine("Enter The Address");
+            contacts1.Address= Console.ReadLine();
+            Console.WriteLine("Enter The City");
+            contacts1.City = Console.ReadLine();
+            Console.WriteLine("Enter The State");
+            contacts1.State = Console.ReadLine();
+            Console.WriteLine("Enter The Zipcode");
+            contacts1.ZipCode = long.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The MobileNo");
+            contacts1.MobNo =long.Parse( Console.ReadLine());
+
+            add.Add(contacts1);
+
+        }
+        public void Display()
+        {
+            foreach (Contacts data in add)
+            {
+                Console.WriteLine("Firstname:" + data.FirstName);
+                Console.WriteLine("Lastname:" + data.LastName);
+                Console.WriteLine("Address:" + data.Address);
+                Console.WriteLine("city:" + data.City);
+                Console.WriteLine("state:" + data.State);
+                Console.WriteLine("Zipcode:" + data.ZipCode);
+                Console.WriteLine("PhoneNumber:" + data.MobNo);
+
+            }
+        }
+        public void UpdateList()
+        {
+
+        }
+    }
+}
---
+++ ProgramAddressBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AddressBook addressBook1 = new AddressBook();
+            addressBook1.Addbook();
+            addressBook1.Display();
+             Console.ReadLine();
+        }
+    }
+}
